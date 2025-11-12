0f000b897ca800d4636c162c85b09a5328c728f2
COULON-DEPUCCIO Kilian <kilian.coulon-depuccio@etu.univ-lyon1.fr>
2025-11-12 21:03:44 +0100

    Ajout du style global

--- src/views/home.php
+++ src/views/home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../public/assets/style/home.css">
+    <link rel="stylesheet" href="./assets/style/global.css">
     <title>Accueil</title>
 </head>
 <body>
