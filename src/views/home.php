f894084da410c2aa00969334332aa9b2e77e300c
leny <leny.arizzi@etu.univ-lyon1.fr>
2025-11-12 19:31:08 +0100

    Développement du home,implémentation de fonction, ajout de la documentation dans HomeModel

--- src/views/home.php
+++ src/views/home.php
@@ -10,21 +10,22 @@
     <h1>Créations populaires</h1>
     <div class=popCreations>
             <?php
-
+                //en attente des nbJaimes
             ?>
     </div>
     <h1>Vos créations</h1>
     <div class=newCreations>
             <?php
+                // attente du js pour faire avec un foreach
                 // foreach ($quiz as $q) {
                 //     echo '<article></article>';
                 // }
-                for ($i=0; $i < 12; $i++) { 
+                for ($i=0; $i < 7; $i++) { 
                     echo '<article>';
-                    echo '<span clas = title>'.$quiz[$i]['title'].'</span>';
-                    echo '<span clas = title>'.$quiz[$i]['description'].'</span>';
-                    echo '<span clas = title>'.$quiz[$i]['title'].'</span>';
-                    echo '<span clas = title>'.$quiz[$i]['title'].'</span>';
+                    echo '<span class = title>'.$quiz[$i]['title'].'</span>';
+                    echo '<span class = description>'.$quiz[$i]['description'].'</span>';
+                    echo '<span class = username> Par '.$quiz[$i]['user_name'].'</span>';
+                    echo '<span class = date> publié le : '.$quiz[$i]['date'].'</span>';
                     echo '</article>';
                 }
             ?>
