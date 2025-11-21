163badf107de14b2e43a14dc84c8ef156741a20b
ARIZZI LENY p2401392 <leny.arizzi@etu.univ-lyon1.fr>
2025-11-21 11:45:59 +0000

    Réorganisation, création de chemin pour quiz et amélioration du css de quiz

--- src/views/Quiz/show.php
+++ src/views/Quiz/show.php
@@ -1,33 +1,33 @@
-<html>
-<head>
-    <link rel="stylesheet" href="quiz.css">
-    <title>Quiz Page</title>
-</html>
-</head>
-<body>
-    <div class ="quiz-réalisation">
-        <button class = "retour"> < Retour</button>   
-        <div class ="question">
-            <h2>Question super difficile</h2>
-            <div class="ensemble-réponse">
-                <div class="réponse">
-                    <input type="checkbox" id="réponse1" name="question1" value="réponse1">
-                    <label for="réponse1">Réponse 1</label>
+<?php
+$title = "Quiz";
+$style = './assets/style/quiz/quiz.css';
+include '../src/views/partials/header.php';
+?>
+
+<div class="quiz-réalisation">
+    <button class="retour">
+        < Retour</button>
+            <div class="question">
+                <h2>Question super difficile</h2>
+                <div class="ensemble-réponse">
+                    <div class="réponse">
+                        <input type="checkbox" id="réponse1" name="question1" value="réponse1">
+                        <label for="réponse1">Réponse 1</label>
+                    </div>
+                    <div class="réponse">
+                        <input type="checkbox" id="réponse2" name="question1" value="réponse2">
+                        <label for="réponse2">Réponse 2</label>
+                    </div>
+                    <div class="réponse">
+                        <input type="checkbox" id="réponse3" name="question1" value="réponse3">
+                        <label for="réponse3">Réponse 3</label>
+                    </div>
+                    <div class="réponse">
+                        <input type="checkbox" id="réponse4" name="question1" value="réponse4">
+                        <label for="réponse4">Réponse 4</label>
+                    </div>
                 </div>
-                <div class="réponse">
-                    <input type="checkbox" id="réponse2" name="question1" value="réponse2">
-                    <label for="réponse2">Réponse 2</label>
-                </div>
-                <div class="réponse">
-                    <input type="checkbox" id="réponse3" name="question1" value="réponse3">
-                    <label for="réponse3">Réponse 3</label>
-                </div>
-                <div class="réponse">
-                    <input type="checkbox" id="réponse4" name="question1" value="réponse4">
-                    <label for="réponse4">Réponse 4</label>
-                </div>
+                <button>Valider</button>
             </div>
-            <button>Valider</button>
-        </div> 
-        </div>
+</div>
 </body>
