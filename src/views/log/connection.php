feb54e364eeac495c8865b7e09ac5caaf4668112
COULON-DEPUCCIO Kilian <kilian.coulon-depuccio@etu.univ-lyon1.fr>
2025-11-24 20:55:03 +0100

    Mise en place des styles des boutons

--- src/views/log/connection.php
+++ src/views/log/connection.php
@@ -9,10 +9,19 @@ require_once '../src/views/partials/header.php';
             <h3><?=$error;?></h3>
         <?php endif;?>
         <form action="?page=log&typelog=connection" method="POST">
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Mot de passe" required>
+            <div class="flatinput">
+                <span></span>
+                <input type="email" name="email" placeholder="Email" required>
+            </div>
+            <div class="flatinput">
+                <span></span>
+                <input type="password" name="password" placeholder="Mot de passe" required>
+            </div>
             <p>Vous n'avez pas de compte ? <a href="?page=log&typelog=register" class=connexion>Inscrivez-vous.</a></p>
-            <button type="submit">Se connecter</button>
+            <button type="submit" class="button">
+                <span></span>
+                <p>Se connecter</p>
+            </button>
         </form>
         <div class="back-link">
             <a href="?page=home">← Retour à l'accueil</a>
