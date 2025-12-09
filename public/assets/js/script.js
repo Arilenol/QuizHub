197f8b400fbc99e9cc28350c26e45c64f0e2b9ba
COULON-DEPUCCIO Kilian <kilian.coulon-depuccio@etu.univ-lyon1.fr>
2025-12-09 13:38:08 +0100

    Mise en place de la PWA

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", () => {
+    if("serviceWorker" in navigator){
+        navigator.serviceWorker.register("sw.js");
+    }
+})
+
 window.addEventListener("DOMContentLoaded", () => {
     initCheckboxSVG();
 });
