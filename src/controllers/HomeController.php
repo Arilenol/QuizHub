1b91c811d925284f73e61bd69cd72fbecfd3fc3b
Leny <tukami69@gmail.com>
2025-12-09 13:48:59 +0100

    devLesson

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 require_once ROOT . '/src/models/HomeModel.php';
 require_once ROOT . '/config/config.php';
+require_once ROOT . '/src/models/LessonModel.php';
 
 class HomeController
 {
@@ -9,11 +10,12 @@ class HomeController
         $db = getDbConnection();
         $model = new HomeModel($db);
         // récupère les données
+        $quiz = $model->getAllInfo();
+
+        // $lessons = 
         if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-            $quiz = $model->getAllInfo();
             $quizNextPart = $model->getAllCreationsByUser($_SESSION['id']);
         } else {
-            $quiz = $model->getAllInfo();
             $quizNextPart = $model->getAllNewCreations();
         }
         // afficher la vue
--- src/controllers/catalogueController.php
+++ src/controllers/catalogueController.php
@@ -1,54 +1,53 @@
 <?php
-    require_once ROOT . '/src/models/CatalogueModel.php';
-    require_once ROOT . '/config/config.php';
+require_once ROOT . '/src/models/CatalogueModel.php';
+require_once ROOT . '/config/config.php';
 
 
-    class CatalogueController {
-        private $model;
+class CatalogueController
+{
+    private $model;
 
-        public function index(){
-            $db = getDbConnection();
-            //constructionBD($db);
-            $this->model = new CatalogueModel($db);
-            // afficher la vue
-            
+    public function index()
+    {
+        $db = getDbConnection();
+        //constructionBD($db);
+        $this->model = new CatalogueModel($db);
+        // afficher la vue
 
-            $cats = $this->model->getCategories();
 
-            $options = [
-                    'date_desc' => 'Date (nouveau → ancien)',
-                    'date_asc' => 'Date (ancien → nouveau)',
-                    'title_asc' => 'Titre (A → Z)',
-                    'title_desc' => 'Titre (Z → A)',
-                    'difficulty_asc' => 'Difficulté (faible → élevé)',
-                    'difficulty_desc' => 'Difficulté (élevé → faible)',
-                    'author_asc' => "Auteur (A → Z)",
-                    'author_desc' => "Auteur (Z → A)",
-                    'genre_asc' => 'Genre (A → Z)',
-                    'genre_desc' => 'Genre (Z → A)'
-                ];
-                
-            $recherche_cat = isset($_GET['categorie'])&& !empty($_GET['categorie'])&& htmlspecialchars($_GET['categorie'])!='Toutes les catégories' ? $_GET['categorie'] : '';
-            $recherche_contenu = isset($_GET['contenu'])&& !empty($_GET['contenu']) ? htmlspecialchars($_GET['contenu']) : '';
-            $recherche_auteur = isset($_GET['searchAuthor']) && !empty($_GET['searchAuthor']) ? htmlspecialchars($_GET['searchAuthor']) : '';
-            $tri = isset($_GET['tri']) && !empty($_GET['tri']) ? $_GET['tri'] : null;
-            if($recherche_cat != ''){
-                $quiz_correspondants = $this->model->searchQuizByAll($recherche_cat,$recherche_contenu,$recherche_auteur,$tri);
-            }else{
-                $quiz_correspondants = $this->model->searchQuizByContentAndAuthor($recherche_contenu,$recherche_auteur,$tri);
-            }
+        $cats = $this->model->getCategories();
 
-            $quizzes = $quiz_correspondants;
-            foreach ($quizzes as $index => $quiz){
-                $quizzes[$index]['categories'] = $this->model->getCategoriesFromQuiz($quiz['id']);
-                $quizzes[$index]['nom_auteur'] = $this->model->getNomAuteur($quiz['user_id']);
-                
-                $quizzes[$index]['likes'] = isset($quiz['likes']) ? (int)$quiz['likes'] : 0;
-                $quizzes[$index]['dislikes'] = isset($quiz['dislikes']) ? (int)$quiz['dislikes'] : 0;
-            }
-            require ROOT . '/src/views/catalogue.php';
+        $options = [
+            'date_desc' => 'Date (nouveau → ancien)',
+            'date_asc' => 'Date (ancien → nouveau)',
+            'title_asc' => 'Titre (A → Z)',
+            'title_desc' => 'Titre (Z → A)',
+            'difficulty_asc' => 'Difficulté (faible → élevé)',
+            'difficulty_desc' => 'Difficulté (élevé → faible)',
+            'author_asc' => "Auteur (A → Z)",
+            'author_desc' => "Auteur (Z → A)",
+            'genre_asc' => 'Genre (A → Z)',
+            'genre_desc' => 'Genre (Z → A)'
+        ];
+
+        $recherche_cat = isset($_GET['categorie']) && !empty($_GET['categorie']) && htmlspecialchars($_GET['categorie']) != 'Toutes les catégories' ? $_GET['categorie'] : '';
+        $recherche_contenu = isset($_GET['contenu']) && !empty($_GET['contenu']) ? htmlspecialchars($_GET['contenu']) : '';
+        $recherche_auteur = isset($_GET['searchAuthor']) && !empty($_GET['searchAuthor']) ? htmlspecialchars($_GET['searchAuthor']) : '';
+        $tri = isset($_GET['tri']) && !empty($_GET['tri']) ? $_GET['tri'] : null;
+        if ($recherche_cat != '') {
+            $quiz_correspondants = $this->model->searchQuizByAll($recherche_cat, $recherche_contenu, $recherche_auteur, $tri);
+        } else {
+            $quiz_correspondants = $this->model->searchQuizByContentAndAuthor($recherche_contenu, $recherche_auteur, $tri);
         }
+
+        $quizzes = $quiz_correspondants;
+        foreach ($quizzes as $index => $quiz) {
+            $quizzes[$index]['categories'] = $this->model->getCategoriesFromQuiz($quiz['id']);
+            $quizzes[$index]['nom_auteur'] = $this->model->getNomAuteur($quiz['user_id']);
+
+            $quizzes[$index]['likes'] = isset($quiz['likes']) ? (int)$quiz['likes'] : 0;
+            $quizzes[$index]['dislikes'] = isset($quiz['dislikes']) ? (int)$quiz['dislikes'] : 0;
+        }
+        require ROOT . '/src/views/catalogue.php';
     }
-
-
-?>
+}
