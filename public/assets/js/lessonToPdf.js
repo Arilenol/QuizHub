59b6fb6751b12958134025fe931ee3294e3eca77
leny <leny.arizzi@etu.univ-lyon1.fr>
2026-01-08 16:01:31 +0100

    Ajout de la fonctionnalité de téléchargement de leçon en pdf

---
+++ public/assets/js/lessonToPdf.js
@@ -0,0 +1,52 @@
+async function save () {
+  const lecon = document.querySelector('.lesson-page')
+  const buttons = document.querySelectorAll('.button, .buttonAction')
+
+  // cacher boutons
+  buttons.forEach(b => (b.style.display = 'none'))
+
+  // sauvegarder styles
+  const oldStyle = {
+    padding: lecon.style.padding,
+    width: lecon.style.width,
+    background: lecon.style.background,
+    boxSizing: lecon.style.boxSizing
+  }
+
+  // styles PDF
+  lecon.style.padding = "15mm"
+  lecon.style.width = "210mm"
+  lecon.style.background = "white"
+  lecon.style.boxSizing = "border-box"
+
+  let title = document.querySelector('.lesson-header h1').textContent.trim()
+  title = title.replace(/\s+/g, '')
+
+  await html2pdf()
+    .from(lecon)
+    .set({
+      margin: 0,
+      filename: `leçon_${title}.pdf`,
+      html2canvas: {
+        scale: 2,
+        scrollX: 0,
+        scrollY: 0,
+      },
+      jsPDF: {
+        unit: 'mm',
+        format: 'a4',
+        orientation: 'portrait'
+      },
+      pagebreak: {
+        mode: ['css', 'avoid-all']
+      }
+    })
+    .save()
+
+  // restaurer styles
+  lecon.style.padding = oldStyle.padding
+  lecon.style.width = oldStyle.width
+  lecon.style.background = oldStyle.background
+  lecon.style.boxSizing = oldStyle.boxSizing
+  buttons.forEach(b => (b.style.display = ''))
+}
