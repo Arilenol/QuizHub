f58b0d4f7d2c6473da4ef0b47f019e38e4d49d5e
COULON-DEPUCCIO Kilian <kilian.coulon-depuccio@etu.univ-lyon1.fr>
2026-01-19 13:14:13 +0100

    Ajout des icônes svg

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -22,7 +22,9 @@
         <form action="index.php" method="GET">
             <button type="button" class="button" id="closeMenu">
             <span></span>
-            <img src="assets/images/cross.svg">
+            <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z" fill="#ffffffff"/>
+            </svg>
         </button>
             <div class="input">
                 <span></span>
@@ -75,10 +77,14 @@
     <header>
         <button class="button" id="openMenu">
             <span></span>
-            <img src="assets/images/burger-menu.svg">
+            <svg width="35px" height="35px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 18L20 18" stroke="#ffffffff" stroke-width="2" stroke-linecap="round"/>
+                <path d="M4 12L20 12" stroke="#ffffffff" stroke-width="2" stroke-linecap="round"/>
+                <path d="M4 6L20 6" stroke="#ffffffff" stroke-width="2" stroke-linecap="round"/>
+            </svg>
         </button>
         <picture onclick="window.location.href='?page=home'" alt="Logo">
-            <source srcset="./assets/images/logo.svg" media="(width >= 1300px)">
+            <source srcset="./assets/images/logo.svg" media="(width >= 1480px)">
             <img src="./assets/images/icon.png">
         </picture>
         <form action="index.php" method="GET">
@@ -290,7 +296,7 @@
     <!-- Création des listeners des boutons du header -->
     <script>
         document.addEventListener('DOMContentLoaded', () => {
-            document.querySelectorAll('header .button').forEach(btn => {
+            document.querySelectorAll('header .button, #menu .button').forEach(btn => {
                 const action = btn.dataset.action;
 
                 if (!action) return;
